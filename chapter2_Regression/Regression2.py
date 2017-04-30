27fc595f46f985288032f51f2fa47e08c6e9248b
hongjinseob <hamunstorys@gmail.com>
2017-04-30 12:40:26 +0900

    modify Regression2.py in chapter2_Regression package

--- chapter2_Regression/Regression2.py
+++ chapter2_Regression/Regression2.py
@@ -18,11 +18,11 @@ vectors_set = []
 for i in range(num_points):
     x1 = np.random.normal(0.0, 0.55)
     y1 = x1 * 0.1 + 0.3 + np.random.normal(0.0, 0.03)
-    vectors_set.append([x1, y1])
+    vectors_set.append([x1, y1]) # vectors_set을 2개의 요소를 가진 벡터로 선언하고, x1을 첫번째 요소에 추가, y1을 두번째 요소에 추가
 
 
-x_data = [v[0] for v in vectors_set]
-y_data = [v[1] for v in vectors_set]
+x_data = [v[0] for v in vectors_set] # vectors_set 의 첫번째 요소를 x_data에 삽입
+y_data = [v[1] for v in vectors_set] # vectors_set 의 두번째 요소를 y_data에 삽입
 
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.zeros([1]))
@@ -44,8 +44,14 @@ for step in range(8):
     print(step, sess.run(loss))
 
     #show graph
+
+    # 산포도 그리기
     plt.plot(x_data, y_data, 'ro')
+    # 직선 그리기
     plt.plot(x_data,sess.run(W)*x_data+sess.run(b))
+
+    #labeling
     plt.xlabel('x')
     plt.ylabel('y')
+
     plt.show()
